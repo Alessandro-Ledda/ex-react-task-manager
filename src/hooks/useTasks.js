56ab49a34c0f0eacb9ad60b73be2cbadb6586f1b
Alessandro Ledda <aleledda75@icloud.com>
2026-05-06 11:13:55 +0200

    create hook " useTasks" + function for operation(add, remove, update)

---
+++ src/hooks/useTasks.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const { VITE_API_URL } = import.meta.env;
+
+function useTasks() {
+
+    const [tasks, setTasks] = usestate([]);
+
+    useEffect(() => {
+        fetch(`${VITE_API_URL}/tasks`)
+            .then(res => res.json())
+            .then(data => setTasks(data))
+            .catch(error => console.error(error))
+            .finally();
+    }, []);
+
+    // settaggio funzioni 
+    // addTask
+    const addTask = (newTask) => { }
+
+    // removeTask
+    const removeTask = (taskId) => { }
+
+    //updateTask
+    const updateTask = (taskId) => { }
+
+    return { tasks, addTask, removeTask, updateTask }
+};
