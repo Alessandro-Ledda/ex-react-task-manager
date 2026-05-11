02560abd5ff7f3fb08f20e801b1370793865de2b
Alessandro Ledda <aleledda75@icloud.com>
2026-05-11 10:46:54 +0200

    feat debouce in file taskList.jsx

--- src/Page/TaskList.jsx
+++ src/Page/TaskList.jsx
@@ -1,8 +1,21 @@
 
 import { ContextApi } from '../Context/ContextApi';
-import { useContext, useState, useMemo } from 'react';
+import { useContext, useState, useMemo, useCallback } from 'react';
 
 import TasksRow from '../components/TasksRow';
+
+// funzione di debounce
+function debounce(callback, delay) {
+
+    let timer;
+    return (value) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            callback(value);
+        }, delay)
+
+    }
+}
 
 function TaskList() {
 
@@ -11,6 +24,12 @@ function TaskList() {
     // creo var di stato per l'ordimanto in base a... con data come riferimento iniziale 
     const [sortBy, setSortBy] = useState('createAt');
     const [sortOrder, setSortOrder] = useState(1);
+
+    // creo var di stato per gestione ricerca task input 
+    const [searchTask, setSearchTask] = useState('');
+
+    // var per debounce
+    const debounceSearch = useCallback(debounce(setSearchTask, 500), []);
 
     // creo var per gestione icona ordine 
     const sortIcon = sortOrder === 1 ? '⇩' : '⇧';
@@ -27,31 +46,40 @@ function TaskList() {
     }
 
     // gestione render 
-    const sortedTask = useMemo(() => {
-        return [...tasks].sort((a, b) => {
-            let comparison;
+    const filteredAndsortedTask = useMemo(() => {
+        return [...tasks].filter((task) => task.title.toLowerCase().includes(searchTask.toLowerCase()))
+            .sort((a, b) => {
 
-            // ordine crescente
-            if (sortBy === 'title') {
-                comparison = a.title.localeCompare(b.title)
-            } else if (sortBy === 'status') {
-                const statusOptions = ["To do", "Doing", "Done"];
-                comparison = statusOptions.indexOf(a.status) - statusOptions.indexOf(b.status);
-            } else if (sortBy === 'createAt') {
-                const dataA = new Date(a.createAt).getTime();
-                const dataB = new Date(b.createAt).getTime();
+                let comparison;
 
-                comparison = dataA - dataB;
+                // ordine crescente
+                if (sortBy === 'title') {
+                    comparison = a.title.localeCompare(b.title)
+                } else if (sortBy === 'status') {
+                    const statusOptions = ["To do", "Doing", "Done"];
+                    comparison = statusOptions.indexOf(a.status) - statusOptions.indexOf(b.status);
+                } else if (sortBy === 'createAt') {
+                    const dataA = new Date(a.createAt).getTime();
+                    const dataB = new Date(b.createAt).getTime();
 
-            }
+                    comparison = dataA - dataB;
 
-            return comparison * sortOrder
-        })
-    }, [tasks, sortBy, sortOrder]);
+                }
+
+                return comparison * sortOrder
+            })
+    }, [tasks, sortBy, sortOrder, searchTask]);
 
     return (
         <>
             <h1>Lista task</h1>
+            {/* input per ricerca task */}
+            <input
+                type="text"
+                placeholder='Cerca Task'
+                // value={searchTask}
+                onChange={(e) => debounceSearch(e.target.value)}
+            />
             <table>
                 <thead>
                     <tr>
@@ -64,7 +92,7 @@ function TaskList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {sortedTask.map((task) => {
+                    {filteredAndsortedTask.map((task) => {
                         return <TasksRow key={task.id} task={task} />
                     })}
                 </tbody>
